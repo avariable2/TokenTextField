ccbcb3eed6acdcef98c7058ba2f3cd81d3943e96
Grande Variable <gvariable@icloud.com>
2024-06-06 14:47:51 +0200

    Initial commit

---
+++ Sources/TokenTextField/TokenModel.swift
@@ -0,0 +1,21 @@
+//
+//  File.swift
+//  
+//
+//  Created by Grande Variable on 06/06/2024.
+//
+
+import Foundation
+import SwiftUI
+
+public struct TokenModel: Identifiable, Hashable, Equatable {
+    
+    public var id: UUID = UUID()
+    public var value: String
+    public var isSelected: Bool = false
+    public var convertedToToken = true
+
+    public static func == (lhs: TokenModel, rhs: TokenModel) -> Bool {
+        return lhs.id == rhs.id && lhs.value == rhs.value && lhs.isSelected == rhs.isSelected && lhs.convertedToToken == rhs.convertedToToken
+    }
+}
